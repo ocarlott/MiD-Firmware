f3a7e8e4482ba12cca690157f0f559c84c1ad9c4
Minh Ngo <michaelngodeveloper@gmail.com>
2019-06-18 08:39:56 -0500

    Updated fingerprint module

---
+++ FingerprintModule.h
@@ -0,0 +1,14 @@
+#ifndef FINGERPRINTMODULE_H
+#define FINGERPRINTMODULE_H
+
+#include <finger.h>
+#include "Constant.h"
+
+class FingerprintModule
+{
+  public:
+	FingerprintModule();
+	void poll();
+};
+
+#endif
